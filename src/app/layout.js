b3c868e4f69c1b07ac6970a4f0ab382a1c29c83d
nitinrathod.tg <nitinrathod.tg@gmail.com>
2024-12-22 16:31:12 +0000

    some changes in logos

--- src/app/layout.js
+++ src/app/layout.js
@@ -66,6 +66,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/logos/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/logos/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/logos/favicon-16x16.png" />
+      </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable} main-body-tag`}>
         <Navigation />
         {children}
