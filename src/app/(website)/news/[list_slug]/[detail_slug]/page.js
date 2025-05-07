2b3afc7789fcb7c209c865b48e5cbeb797c435ba
Nitin Rathod <nitin.rathod@nividous.com>
2025-05-07 16:40:40 +0530

    OG images added to news detail page.

--- src/app/(website)/news/[list_slug]/[detail_slug]/page.js
+++ src/app/(website)/news/[list_slug]/[detail_slug]/page.js
@@ -3,12 +3,36 @@ import { getNewsDetail } from '@/utils/services/services';
 import React from 'react'
 import { Container } from 'react-bootstrap'
 
-export  const generateMetadata = async({params})=>{
+export const generateMetadata = async ({ params }) => {
     const { detail_slug } = params;
     const { data } = await getNewsDetail(detail_slug);
     return {
         title: data?.name,
         description: data?.short_description,
+
+        openGraph: {
+            type: "website",
+            url: "https://jalnalokjyot.vercel.app",
+            title: data?.name,
+            description:
+                data?.short_description,
+            images: [
+                {
+                    url: data?.image,
+                    width: 1200,
+                    height: 630,
+                    alt: data?.name,
+                },
+            ],
+            siteName: "Jalna Lokjyot",
+        },
+        twitter: {
+            card: "summary_large_image",
+            site: "Jalna Lokjyot",
+            title: data?.name,
+            description: data?.short_description,
+            images: [data?.image],
+        },
     }
 }
 
